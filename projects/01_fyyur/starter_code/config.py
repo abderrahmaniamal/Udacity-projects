d991272bcc6424347e1f6f721fc2cf5cd79a629c
Amal Abderr <abderr01@ads.uni-passau.de>
2022-03-08 10:58:48 +0100

    fyyur project

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -1,4 +1,7 @@
 import os
+
+import sqlalchemy
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -6,8 +9,16 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
 # Connect to the database
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+
+
+DATABASE_URL =  'postgresql://postgres:Lamlouma@localhost:5432/DB'
+#SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = DATABASE_URL
+
